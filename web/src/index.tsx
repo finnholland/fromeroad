a185f7b1c58bf1415f600867971ee969d5a96f15
fudgedygut <65142636+fhllnd@users.noreply.github.com>
2022-12-24 09:40:10 +1030

    api and tokens

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Login from './screens/Login';
+import ErrorPage from './Error';
+import Home from './screens/Home';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+    errorElement: <ErrorPage/>,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <Login />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
--- web/src/screens/Login.tsx
+++ web/src/screens/Login.tsx
@@ -1,23 +1,61 @@
 import Axios from 'axios'
 import React, { useState } from 'react'
+import { Link } from "react-router-dom";
+
 
 const Login = () => {
+  
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [company, setCompany] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
-  const signUp = () => {
+
+  const signUp = async () => {
     if (password !== confirmPassword || password === '' || !password) {
       alert('passowrds no matchy');
       return;
     }
-    Axios.post('http://localhost:9000/user/createUser', {
+    if (name === '' || email === '' || company === '') {
+      alert('form must be filled!');
+      return;
+    }
+    Axios.post('http://localhost:9000/user/signup', {
       name: name,
       email: email,
       company: company,
       password: password
+    }).catch(function (error) {
+      console.log(error)
+    }).then((res) => {
+      console.log(res)
+    })
+  }
+
+  const login = async () => {
+    Axios.post('http://localhost:9000/user/login', {
+      email: email,
+      password: password
     }).then(res => {
+      if (res.status !== 200) {
+        alert('incorrect email or password')
+      } else {
+        // <Link to={`/`}>Your Name</Link>
+      }
+      localStorage.setItem('token', res.data.token)
+      console.log(res)
+    })
+  }
+
+  const getTest = async () => {
+    Axios.get('http://localhost:9000/user/2', {
+      headers: {
+        authorisation: `Bearer ${localStorage.getItem('token')}`
+      }
+    }).then(res => {
+      if (res.status !== 200) {
+        alert('incorrect email or password')
+      }
       console.log(res)
     })
   }
@@ -29,8 +67,9 @@ const Login = () => {
       <input placeholder='Company' value={company} onChange={(e) => setCompany(e.target.value)} />
       <input placeholder='Password'value={password} onChange={(e) => setPassword(e.target.value)} />
       <input placeholder='Confirm Password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-      <button onClick={() => signUp()}>log in</button>
-      <button onClick={() => signUp()}>log in</button>
+      <button onClick={() => signUp()}>sign up</button>
+      <button onClick={() => login()}>log in</button>
+      <button onClick={() => getTest()}>test</button>
       
     </div>
 
