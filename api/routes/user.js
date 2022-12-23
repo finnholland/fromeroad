f0e7eeec5b608b6e4eed336b5bfbaa9e62a576de
fudgedygut <65142636+fhllnd@users.noreply.github.com>
2022-12-24 09:37:58 +1030

    login tokens working + validation

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,15 +1,27 @@
+require('dotenv').config();
+
 var express = require('express');
 app = express()
 var db = require('..');
 const cors = require('cors')
-const bcrypt = require("bcrypt") 
+const bcrypt = require("bcrypt")
+const CryptoJS = require("crypto-js")
+const jwt = require('jsonwebtoken');
+const salt = 10
 app.use(cors());
+const keys = require('../privatepublickey');
 
-// api/user/{route}
+
+// ROUTES RELATING TO USER ~~~~ api/user/{route}
 
 // get user by ID
 app.get('/:id', (req, res) => {
-  db.query(`select * from users where userID = ${req.params.id}`, (err, result, fields) => {
+  let token = req.headers['authorisation']
+  token = token && token.split(' ')[1]
+  console.log(JSON.stringify(req.headers));
+  const decryptToken = jwt.verify(token, process.env.SECRET, { algorithms: ["HS256"] });
+  console.log("\nJWT verification result: " + JSON.stringify(decryptToken));
+  db.query('select * from users where userID = ?', [req.params.id], (err, result, fields) => {
     if (err) {
       console.log('error occurred: '+ err)
     } else {
@@ -19,30 +31,71 @@ app.get('/:id', (req, res) => {
 })
 
 // get user by password
-app.get('/login', async (req, res) => {
-  const name = req.body.name;
-  const password = await bcrypt.hash(req.body.password, 10);
-  db.query(`select * from users where email = ${name} and password = ${password}`, (err, result, fields) => {
+app.post('/login', async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  db.query('select * from users where email = ?', email, (err, result, fields) => {
     if (err) {
-      console.log('error occurred: '+ err)
+      console.log('error occurred: ' + err)
+      res.sendStatus(err.code)
+    } else if (result.length <= 0) {
+      res.sendStatus(401).send({
+          msg:'email or password is incorrect'
+      })
     } else {
-      res.send(result)
+      bcrypt.compare(password, result[0].password, function (err, valid) {
+        if (err) {
+
+        }
+        if (valid) {
+          const encryptToken = jwt.sign({ userID: result[0].userID.toString() }, process.env.SECRET, { algorithm: 'HS256' });
+          return res.status(200).send({
+              msg: "logged in successfully",
+              user: result[0],
+              token: encryptToken,
+          })
+        }
+        else {
+          return res.status(401)
+        }
+      })
     }
   })
 })
 
 // create user
-app.post("/createUser", async (req, res) => {
-  const name = req.body.name;
-  const email = req.body.email;
-  const company = req.body.company;
-  const password = await bcrypt.hash(req.body.password, 10);
-  console.log(password)
-  db.query('insert into users (name, email, company, password) values (?,?,?,?)', [name, email, company, password], (err, result, fields) => {
-    if (err) {
-      console.log('error occurred: '+ err)
-    } else {
-      res.send(result)
+app.post('/signup', async (req, res) => {
+  const user = {
+    name: req.body.name,
+    email: req.body.email,
+    company: req.body.company,
+    password: req.body.password,
+  }
+
+  db.query('select * from users where email = ?', user.email, (err, result) => { 
+    if (err) throw (err)
+    if (result.length != 0) {
+      res.sendStatus(409)
+    }
+    else {
+      bcrypt.hash(user.password, 10).then((hash) => { 
+        user.password = hash;
+        db.query('insert into users (name, email, company, password) values (?,?,?,?)', [user.name, user.email, user.company, user.password], (err, result, fields) => {
+          if (err) throw (err)
+        })
+      
+        db.query('SELECT * FROM users WHERE email = ?', user.email, (err,result) => {
+          if (err) {
+            return res.status(400).send({
+                msg:err
+            })
+          }
+          return res.status(201).send({
+              user: user,
+              msg:"successfully registered"
+            })
+        })
+      })
     }
   })
 })
