7f61d5ff601adcca97f87ec8d2588cc78ceb4c2d
finn <65142636+fhllnd@users.noreply.github.com>
2023-01-09 12:57:27 +1030

    svgadd upvote button

--- web/src/assets/svg/SvgAddButton.tsx
+++ web/src/assets/svg/SvgAddButton.tsx
@@ -8,8 +8,8 @@ export const SvgAddButton: React.FC<React.SVGProps<SVGSVGElement>> = (props: any
     {...props}
   >
     <circle cx="20" cy="20" r="19.5" fill="white" style={{stroke: props.fill, fill: "white"}}/>
-    <path d="M29 20L11 20" stroke-linecap="round" style={{stroke: props.stroke}}/>
-    <path d="M20 29L20 11" stroke-linecap="round" style={{stroke: props.stroke}}/>
+    <path d="M29 20L11 20" stroke-linecap="round" style={{stroke: props.stroke, strokeWidth: props.strokeWidth}}/>
+    <path d="M20 29L20 11" stroke-linecap="round" style={{stroke: props.stroke, strokeWidth: props.strokeWidth, visibility: props.fontVariant}}/>
   </svg>
 );
 export default SvgAddButton;
