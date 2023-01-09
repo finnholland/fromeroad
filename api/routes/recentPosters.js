8514e214d373473ea76e97b3fe613aa6dc96b706
fudgedygut <65142636+fhllnd@users.noreply.github.com>
2023-01-09 21:51:15 +1030

    sql

--- api/routes/recentPosters.js
+++ api/routes/recentPosters.js
@@ -8,6 +8,7 @@ app.get('/', (req, res) => {
   db.query(`select users.userID, name, company, profileImageUrl, postCount, lastPost from users
             left join (select userID, COUNT(posts.userID) as postCount, max(createdAt) as lastPost from posts group by userID) recentPosts
             on users.userID = recentPosts.userID
+            where users.userID in (select userID from posts)
             order by postCount desc limit 5;`, (err, result, fields) => {
     if (err) {
       console.log('error occurred: '+ err)
