2991ae20c1522844dd6c3aa899ba7a26cc16c3ee
fudgedygut <65142636+fhllnd@users.noreply.github.com>
2023-02-24 11:28:45 +1030

    docker scripting, environment vars.
    created a docker script to delete and rebuild api image and containers. also added env vars for dev and prod services seperately. and AWS env for the api key, hopefully working.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: "fromeroad_mysql",
+    host: process.env.NODE_ENV+"_fromeroad_mysql",
     // host: "localhost",
     port: 3306,
     user: "fromeroadadmin",
