0f48a8520f05699e1064455a6d8e8a8402f3bfec
fudgedygut <65142636+fhllnd@users.noreply.github.com>
2023-02-27 17:35:22 +1030

    react_app

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,4 +1,4 @@
-const API = process.env.REACT_API_KEY;
+const API = process.env.REACT_APP_API_KEY;
 // const API = 'http://localhost'
 const HOUR = 60000 * 60;
 const EIGHT_MEGABYTES = 1048576 * 8; // 1mb * 8 in bytes
