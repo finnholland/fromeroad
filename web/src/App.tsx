5f8f8eca3305b5b58c5dbb17f95385aedf65eb05
fudgedygut <65142636+fhllnd@users.noreply.github.com>
2023-02-27 17:23:10 +1030

    update env

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -24,6 +24,7 @@ function App() {
     if (localStorage.getItem('token') && localStorage.getItem('token') !== '') {
       setAuthenticated(true)
     }
+    console.log(API)
     getUserFromToken()
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,4 +1,4 @@
-const API = 'https://api.fromeroad.com';
+const API = process.env.REACT_API_KEY;
 // const API = 'http://localhost'
 const HOUR = 60000 * 60;
 const EIGHT_MEGABYTES = 1048576 * 8; // 1mb * 8 in bytes
