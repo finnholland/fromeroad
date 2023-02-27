ff94333410ce699182f13dc277b5382929a0f783
fudgedygut <65142636+fhllnd@users.noreply.github.com>
2023-02-27 19:42:10 +1030

    constants obj

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,5 +1,12 @@
-const API = process.env.REACT_APP_ENV_API_KEY;
-// const API = 'http://localhost'
+
+const API_URLS = {
+  env: process.env.REACT_APP_ENV_API_KEY,
+  dev: 'https://dev.api.fromeroad.com',
+  prod: 'https://dev.api.fromeroad.com',
+  local: 'http://localhost'
+}
+const API = API_URLS.local;
+
 const HOUR = 60000 * 60;
 const EIGHT_MEGABYTES = 1048576 * 8; // 1mb * 8 in bytes
 const DEFAULT_PROFILE_IMAGE = '/data/default/default_profile_image.jpg';
