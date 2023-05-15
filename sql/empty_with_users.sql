d18b83c8251f5cf868411daeae6261a0305f4d39
Finn <65142636+fhllnd@users.noreply.github.com>
2023-05-15 15:56:47 +0930

    Exports & password encryption (#28)
    * exports and ps1
    
    * crypto

--- sql/empty_with_users.sql
+++ sql/empty_with_users.sql
@@ -148,20 +148,6 @@ CREATE TABLE `userinterests` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `userposts`
---
-
-DROP TABLE IF EXISTS `userposts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `userposts` (
-  `userID` int NOT NULL,
-  `postID` int NOT NULL,
-  PRIMARY KEY (`userID`,`postID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Table structure for table `users`
 --
 
