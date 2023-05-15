c60cfea48a1d01a2f822add1373dd486324b3495
finn <65142636+fhllnd@users.noreply.github.com>
2023-05-15 14:36:35 +0930

    crypto

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -23,7 +23,8 @@ const HOUR = 60000 * 60;
 const EIGHT_MEGABYTES = 1048576 * 8; // 1mb * 8 in bytes
 const DEFAULT_PROFILE_IMAGE = '/data/default/default_profile_image.jpg';
 const JWT_TOKEN = `Bearer ${localStorage.getItem('token')}`
+const CRYPTO_KEY = process.env.REACT_APP_CRYPTO_KEY || 'c973dcf40aa4860d' // local dev 
 
 const PLACEHOLDERS = ['hello moon - 🌏', 'hello moon - 🌏', 'pasted links will be shortened :)', 'you can tag; words with ;']
 
-export {API, PLACEHOLDERS, EIGHT_MEGABYTES, HOUR, DEFAULT_PROFILE_IMAGE, JWT_TOKEN, S3_BUCKET}
+export {API, PLACEHOLDERS, EIGHT_MEGABYTES, HOUR, DEFAULT_PROFILE_IMAGE, JWT_TOKEN, CRYPTO_KEY, S3_BUCKET}
