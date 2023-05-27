008cbb08b8ea4057f060fd999013804c3216b2f2
Finn <65142636+fhllnd@users.noreply.github.com>
2023-05-27 23:24:08 +0930

    updated secg for 443 to 8080 (lb to the to ECS)

--- terraform/main.tf
+++ terraform/main.tf
@@ -178,7 +178,7 @@ resource "aws_security_group" "security_group" {
   }
   ingress {
     from_port   = 443
-    to_port     = 8443
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description      = ""
